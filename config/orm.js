2ee140c24b3f4870e0a628edcdea505414d8fea5
Monica Szabo <monicaszb@gmail.com>
2016-07-29 13:25:37 -0500

    adding styling

--- config/orm.js
+++ config/orm.js
@@ -43,10 +43,7 @@ var orm = {
 		});
 	},
 	delete: function (table, condition, cb) {
-		var queryString = 'DELETE FROM ' + table;
-		queryString = queryString + ' WHERE ';
-		queryString = queryString + condition;
-
+		var queryString = 'DELETE FROM ' + table + ' WHERE ' + condition;
 		connection.query(queryString, function (err, result) {
 			if (err) throw err;
 			cb(result);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,6 @@ router.get('/', function (req, res) {
 router.get('/burgers', function (req, res) {
 	burger.all(function (data) {
 		var hbsObject = { burgers: data };
-		console.log(hbsObject);
 		res.render('index', hbsObject);
 	});
 });
@@ -22,8 +21,6 @@ router.post('/burgers/create', function (req, res) {
 
 router.put('/burgers/update/:id', function (req, res) {
 	var condition = 'id = ' + req.params.id;
-
-	console.log('condition', condition);
 
 	burger.update({ devoured: req.body.devoured }, condition, function () {
 		res.redirect('/burgers');
